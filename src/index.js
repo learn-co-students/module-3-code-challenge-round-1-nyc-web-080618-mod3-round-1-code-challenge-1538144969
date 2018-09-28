3dc4c6c6d7bbe6dd3df24e6b6c0f24c789dd5951
Richard Chen <richard.c.chen@gmail.com>
2018-09-28 11:59:35 -0400

    finshed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const yourUUID = 1 //Enter your assigned uuid here
-
+  const yourUUID = '44a7847f-df37-4a45-ba62-286d3fbcbf74' //Enter your assigned uuid here
   const imageURL = `https://randopic.herokuapp.com/images/${yourUUID}`
-  
-  let imageId = 1 //Enter the id from the fetched image here
-
+  let imageId = 920 //Enter the id from the fetched image here
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  const imageDisplay = document.getElementById('image_card')
+  const imageContainer = document.getElementById('entire')
+
+  return fetch(`https://randopic.herokuapp.com/images/${yourUUID}`)
+  .then((resp)=> resp.json())
+  .then(json => ((item) =>
+  {
+    imageDisplay.innerHTML = `<h1>testing</h1>`
+  }))
+
+
+  // document.addEventListener("click", (event) =>{
+  //   console.log(event.target);
+  //   fetch(`https://randopic.herokuapp.com/images/${yourUUID}`), {
+  //     method: 'PATCH',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(Obj)
+  //   }).then(resp => resp.json())
+  //   } //end of listener
 
 
 
-})
+
+
+
+
+}) //end of class
