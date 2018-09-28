a185fdcfc334dbacb1673fe36d927a6cddadc5a0
github username <ariellenramirez@gmail.com>
2018-09-28 12:01:09 -0400

    done

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,20 @@
+let commentArray = []
+
 class Comment {
-  
+  constructor(commentObj){
+    this.comment = commentObj.comment
+    this.id = commentObj.id
+    commentArray.push(this)
+  }
+
+
+  static findById(givenId){
+    return commentArray.find(function(comment){
+      return comment.id == givenId
+      
+    })
+
+
+  }
+
 }
--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,19 @@
+let imageArray = []
+
 class Image {
+  constructor(imageObj){
+   this.id = imageObj.id
+   this.url = imageObj.url
+   this.name = imageObj.name
+   this.like_count= imageObj.like_count
+   this.comments = imageObj.comments
+   imageArray.push(this)
+
+  }
+
+
   
+
+
 
 }
