c3ec75bead48fb4963c0f7dc9a7cd5e974a080b5
github username <bobjin42@gmail.com>
2018-09-28 12:00:30 -0400

    done

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,29 @@
 class Image {
-  
+
+  static find(id){
+    return Image.all.find((image) => image.id == id);
+  }
+
+  constructor(imageObj){
+    this.id = imageObj.id;
+    this.url = imageObj.url;
+    this.name = imageObj.name;
+    this.like_count = 0;
+    this.comments = imageObj.comments;
+    Image.all.push(this);
+  }
+
+  render(){
+    return `
+    <img src="${this.url}" id="image" data-id-${this.id}>
+    <h4 id="name">${this.name}</h4>
+    <span>Likes:
+      <span id="targetLikes">${this.like_count}</span>
+    </span>
+    <button id="like_button" data-id="${this.id}">Like</button>
+    `
+  }
 
 }
+
+Image.all = []
