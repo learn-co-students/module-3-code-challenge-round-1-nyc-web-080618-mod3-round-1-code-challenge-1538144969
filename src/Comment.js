a9a964553463fe7563adcf07f030bcafb3d0f7db
github username <github email address>
2018-09-28 11:59:39 -0400

    done

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,14 @@
 class Comment {
-  
+  constructor(obj){
+    this.id = obj.id
+    this.content = obj.content
+    this.image_id = obj.image_id
+  }
+  render() {
+    return (
+      `
+      <li id="${this.id}">${this.content}</li>
+      `
+    )
+  }
 }
