650e4f335531b258b10d451d76160f9a8f89ee73
Justin Wang <justinw0827@gmail.com>
2018-09-28 11:59:40 -0400

    Done

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,27 @@
+const allComments = []
+
 class Comment {
-  
+  static renderAll() {
+    return allComments.map(comment => {
+      return comment.render()
+    }).join('')
+  }
+
+  static getAllComments() {
+    return allComments
+  }
+
+  constructor(commentObj) {
+    this.id = commentObj.id
+    this.content = commentObj.content
+    allComments.push(this)
+  }
+
+  render() {
+    return `
+      <br>
+      <li>${this.content}   <button data-id="${this.id}">Delete</button></li>
+      <br>
+    `
+  }
 }
