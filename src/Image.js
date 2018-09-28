ec73c4fec60ed253d098cda82ea9e992e5e4955a
bwadlow <bwadlow@gmail.com>
2018-09-28 12:00:56 -0400

    Done

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,23 @@
+allImages=[]
+
 class Image {
-  
+  constructor(obj){
+    this.id = obj.id
+    this.url = obj.url
+    this.name = obj.name
+    this.comments = obj.comments
+    this.like_count = obj.like_count
+    allImages.push(this)
+  }
+
+  render(){
+    return (`
+      <img src="${this.url}">
+      <h4>${this.name}</h4
+      `)
+    }
+
+
+
 
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  const yourUUID = 1 //Enter your assigned uuid here
-
+  const yourUUID = '59e0685b-ea20-489c-aba6-b649024fbdf3'
   const imageURL = `https://randopic.herokuapp.com/images/${yourUUID}`
-  
-  let imageId = 1 //Enter the id from the fetched image here
-
+  let imageId = 914
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const imageContainer = document.getElementById('image_content')
+  const imageCard = document.getElementById('image_card')
+  const likeButton = document.getElementById('like_button')
+  const likeCount = document.getElementById('likes').value
 
+  fetch(`https://randopic.herokuapp.com/images/${yourUUID}`)
+  .then(response => response.json())
+  .then(imageObj => {
+    let newImage = new Image(imageObj)
+    imageContainer.innerHTML += newImage.render()
+  })
+
+  imageCard.addEventListener('click', (event) =>{
+    if(event.target.id === "like_button") {
+      likeCount +=1
+
+
+      fetch(likeURL, {
+        method:'POST',
+        headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        },
+        body:JSON.stringify({
+          image_id:imageId
+          like_count:likeCount
+        })
+      })
+    }
+  })
 
 })
+
+// Didn't spend enough time studying optimistic rendering,
+// spent too much time trying to figure out how
+// to increase likes.
